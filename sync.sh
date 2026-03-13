62ec5c909521d88de5fb1dc004d24c6eefbb534c
gearonixx <egor.uzhanin@gmail.com>
2026-03-13 12:42:23 +0300

    update v0.5

--- sync.sh
+++ sync.sh
@@ -14,7 +14,7 @@ rm -rf ~/tm/t14p/.bashrc
 cp -r ~/.local/share/omarchy/default/hypr ~/tm/t14p/hypr/default
 cp -r ~/.config/hypr ~/tm/t14p/hypr/config
 cp -r ~/.config/waybar ~/tm/t14p/waybar
-cp -r ~/.config/mako ~/tm/14p/mako
+cp -r ~/.config/mako ~/tm/t14p/mako
 
 cp ~/.bashrc ~/tm/t14p/.bashrc
 
